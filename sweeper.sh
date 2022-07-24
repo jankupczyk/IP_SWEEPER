86b13c9ad1e294e2869913ebf3ac21a8b4b5bf7e
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2022-07-24 21:40:06 +0200

    Add files via upload

--- sweeper.sh
+++ sweeper.sh
@@ -2,7 +2,7 @@
 clear
 
 author="©Jan Kupczyk"
-version="1.14.1"
+version="2.0.1"
 
 fg_red=`tput setaf 1`
 fg_green=`tput setaf 2`
@@ -12,29 +12,38 @@ fg_magenta=`tput setaf 5`
 fg_cyan=`tput setaf 6`
 fg_white=`tput setaf 7`
 fg_def_col="\033[00m"
+bg_red='\033[41m'
+bg_black='\033[40m'
 
 S_KEY=$(shuf -er -n7  {A..Z} {a..z} {0..9} | tr -d '\n')
 BOD=57
-PVN=190
-DV=232
+PVN=222
+DV=264
 
 log=$(date +"%T")
+
+DO_NOT_SCAN_IP=("6.0.0" "7.0.0" "7.0.0" "11.0.0" "21.0.0" "22.0.0" "26.0.0" "28.0.0" "29.0.0" "30.0.0" "33.0.0" "55.0.0" "205.0.0" "214.0.0" "215.0.0" "150.207.2" "140.35.155" "163.12.0" "81.160.0" "162.45.0" "195.101.232" "92.43.123" "87.54.11" "153.31.0" "192.84.170" "192.217.228" "195.10.0" "198.190.209" "205.96.0" "205.229.233" "207.43.55" "207.60.0" "209.35.0" "209.122.130" "212.159.33" "217.6.0" "194.42.114" "195.128.0" "217.243.168" "168.175.70" "168.175.170" "168.175.171" "168.175.172" "202.108.0" "195.101.232" "153.31.0" "166.64.0" "164.112.0" "165.187.0" "198.61.8" "192.190.61" "203.25.230" "192.251.207" "162.45.0" "206.212.128")
+
+DONT_DO_THAT_header="--18 U.S. Code § 1030 - Fraud and related activity in connection with computers"
+DONT_DO_THAT_message="----DO NOT SCAN!! - What you want to do now is highly illegal and prosecutable!"
 
 printf "${fg_red}
 ╔═╗╦ ╦╔═╗╔═╗╔═╗╔═╗╦═╗
 ╚═╗║║║║╣ ║╣ ╠═╝║╣ ╠╦╝
 ╚═╝╚╩╝╚═╝╚═╝╩  ╚═╝╩╚═v${version}
 "
-echo -e "\n${fg_red}Protocol version:${PVN}\nData version:${DV}${fg_white}\n"
+
+echo -e "${fg_white}\n${bg_red}DISCLAIMER: Remember that you use the script at your own risk, the author of script is not responsible for any potential damage and will not be liable!${fg_def_col}${fg_white}"
+
 echo -e "\n${fg_red}Be aware that the program is in its alpha version, so there may be bugs!${fg_white}"
 echo -e "${fg_red}Ping sweep is a method that can establish a range of IP addresses which map to live hosts.${fg_white}"
 echo -e "${fg_red}Be aware that pings can be detected by protocol loggers!${fg_white}"
-echo -e "${fg_red}Remember that you use the script at your own risk, the author is not responsible for any potential damage!${fg_white}\n\n"
 echo -e "${fg_green}Complete only the octets responsible for the network address, and leave the last one responsible for the host address empty!${fg_white}"
 echo -e "${fg_green}Enter the ip address in this format |xxx.xxx.xxx|${fg_white}"
 read -p "${fg_green}Enter network address: ${fg_white}" IP_input
-if [[ ${IP_input} == "" ]];then
-    echo -e "${fg_red}Please provide valid IP adress!${fg_white}"
+
+if [[ ${DO_NOT_SCAN_IP[*]} =~ ${IP_input} ]];then
+    echo -e "\n${bg_red}${DONT_DO_THAT_header}\n${DONT_DO_THAT_message}\n------Youre tried to ping [${IP_input}.0] address that belongs to military or gov facility!!!!!! ${fg_def_col}${fg_white}"
 else
     echo -e "\n${fg_red}Running ${0} ${fg_white}" && sleep 2s
     echo -e "${fg_red}"
